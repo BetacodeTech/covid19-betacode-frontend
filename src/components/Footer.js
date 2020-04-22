3e4bc5a974b3a8a6dbe2ff305fb2f0a21e1aa567
Andre <rosado_andre@live.com.pt>
2020-04-22 14:50:57 +0100

    First commit

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div style={styles.footer}>
+            <h3>Footer</h3>
+        </div>
+    );
+}
+
+const styles = {
+    footer: {
+        position: "fixed",
+        color: "red"
+    }
+}
+
+export default Footer;
